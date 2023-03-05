acf322766486d8a28f7d32ee624b46df48fba6bb
guejo8 <guejo432@gmail.com>
2023-03-05 11:02:25 +0100

    he creado componente barra navegacion.vue asi como su archivo de test, este contiene dos test muy sencillos, ambos passed. Agregados y comprobados algunos test para el formulario, todos ellos estado passed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <HelloWorld />
   <PrimerComponente />
+  <BarraNavegacion />
   <FormularioContacto />
   <ImagenesComponente />
 </template>
@@ -8,14 +9,17 @@
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import PrimerComponente from "./components/PrimerComponente.vue";
+import BarraNavegacion from "./components/BarraNavegacion.vue";
 import FormularioContacto from "./components/FormularioContacto.vue";
 import ImagenesComponente from "./components/ImagenesComponente.vue";
+
 
 export default {
   name: "App",
   components: {
     HelloWorld,
     PrimerComponente,
+    BarraNavegacion,
     FormularioContacto,
     ImagenesComponente,
   },
---
+++ src/components/BarraNavegacion.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="barra-navegacion">
+    <img class="logo" src="../assets/logo.png" alt="Logo de Vue">
+    <div class="opciones">
+      <div class="opcion">Conócenos</div>
+      <div class="opcion">Galería</div>
+      <div class="opcion">Home</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BarraNavegacion'
+}
+</script>
+
+<style>
+.barra-navegacion {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #00688b;
+  padding: 1%;
+  border: 0.2rem solid white;
+}
+
+.logo {
+  height: 3rem;
+  width: 3rem;
+}
+
+.opciones {
+  display: flex;
+  gap: 3%;
+  font-size: 1.2rem;
+}
+
+.opcion {
+  color: white;
+  margin-right: 2%;
+  border: 0.1rem solid white;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+}
+
+.opcion-borde:last-child {
+  margin-right: 0;
+}
+
+.opcion:hover {
+  background-color: white;
+  color: #7fffd4;
+}
+</style>
