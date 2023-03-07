3a3fa8eacf7a59da7998b6094c65e12de8dfd6e7
bzmay0149 <mayjuanbaticono@gmail.com>
2023-03-07 08:41:39 +0100

    crear FooterComponente.spec.js

---
+++ tests/unit/FooterComponente.spec.js
@@ -0,0 +1,17 @@
+import { mount } from "@vue/test-utils";
+import  FooterComponente  from "@/components/FooterComponente.vue";
+describe("FooterComponente", () =>{
+    
+
+    test('en la lista del footer existe Información legal', () => {
+        const lista = ['Información legal', 'Politica de privacidad',];
+        expect(lista).toContain('Información legal');
+        expect(new Set(lista)).toContain('Información legal');
+    });
+    test('en la lista del footer existe Politica de privacidad', () => {
+        const lista = ['Información legal', 'Politica de privacidad',];
+        expect(lista).toContain('Politica de privacidad');
+        expect(new Set(lista)).toContain('Politica de privacidad');
+    });
+
+});
